e6e8fcf60becfa710909c3afbc48004aa1db0a0f
João Silva <joaogomesdev32@gmail.com>
2024-08-24 21:06:52 +0100

    Nav/Footer
    Finalizando o cabeçalho e o footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <p>Footer</p>
+    <div id="footer">
+        <p>Make Your Burger &copy; 2024</p>
     </div>
 </template>
 
@@ -10,6 +10,15 @@
     }
 </script>
 
-<style>
-
+<style scoped>
+    #footer {
+        height: 200px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: #fcba03;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+    }
 </style>
